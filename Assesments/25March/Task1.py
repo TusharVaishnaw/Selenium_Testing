7e2cd7eb77c99c852b5ed79b895708b8f4a17606
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-28 00:35:06 +0530

    Update Task1.py

--- Assesments/25March/Task1.py
+++ Assesments/25March/Task1.py
@@ -2,7 +2,6 @@ import os
 import time
 from time import sleep
 from selenium.webdriver import Chrome, ChromeOptions
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
 options = ChromeOptions()
@@ -13,13 +12,13 @@ driver.implicitly_wait(10)
 
 driver.get("https://in.pinterest.com/")
 
-folder = os.path.join(os.getcwd(), 'practice')
+folder = os.path.join(os.getcwd(), 'screenshotz')
 os.makedirs(folder, exist_ok=True)
 
 time_stamp = time.strftime("%Y%m%d%H%M%S")
 
 #ss1
-driver.save_screenshot(f'{folder}/entirePage_{time_stamp}.png')
+driver.save_screenshot(f'{folder}/fullpage_ss_{time_stamp}.png')
 
 sleep(2)
 
@@ -28,11 +27,9 @@ ele = driver.find_element(
     '//*[@id="mweb-unauth-container"]/div/div/div/div[4]/div[2]/div/div/div/div[2]/div/div/img'
 )
 
-action = ActionChains(driver)
-action.move_to_element(ele).pause(2).perform()
 
 #ss2
-ele.screenshot(f'{folder}/Pinterest_{time_stamp}.png')
+ele.screenshot(f'{folder}/element_img_ss_{time_stamp}.png')
 
 sleep(5)
 driver.quit()
