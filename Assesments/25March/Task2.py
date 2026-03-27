58f789520a38bcf9e2b1b4b2f17f6fdcf3fd0230
Tushar Vaishnaw <vaishnawtushar@gmail.com>
2026-03-28 00:41:44 +0530

    Update Task2.py

--- Assesments/25March/Task2.py
+++ Assesments/25March/Task2.py
@@ -13,7 +13,6 @@ driver = Chrome(options=options)
 driver.implicitly_wait(10)
 
 driver.get('https://www.lenskart.com/')
-driver.maximize_window()
 driver.find_element(By.XPATH, "//a[@id='lrd1']").click()
 
 expected = 'https://www.lenskart.com/eyeglasses.html'
@@ -24,10 +23,10 @@ driver.find_element(By.XPATH, "//select[@id='sortByDropdown']").click()
 driver.find_element(By.XPATH, "//option[.='Most Viewed']").click()
 
 # ss
-folder = os.path.join(os.getcwd(), 'Lenskart_practice')
+folder = os.path.join(os.getcwd(), 'screenshotz')
 os.makedirs(folder, exist_ok=True)
 time_stamp = time.strftime("%Y%m%d%H%M%S")
-driver.save_screenshot(f'{folder}/lenskart_{time_stamp}.png')
+driver.save_screenshot(f'{folder}/Task2{time_stamp}.png')
 
 sleep(5)
 driver.close()
